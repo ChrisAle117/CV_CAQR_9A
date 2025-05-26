dfd08f00beda2bf0d3e60c328d7cb322eea9b1c1
ChrisAle117 <20223tn105@utez.edu.mx>
2025-05-26 17:59:07 -0600

    [ADD] Se agrego el controller User controller.java

---
+++ src/main/java/mx/edu/utez/cvcaqr9a/Controller/User1Controller.java
@@ -0,0 +1,14 @@
+package mx.edu.utez.cvcaqr9a.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user1")
+public class User1Controller {
+    @GetMapping("")
+    public String getMessage() {
+        return "Hello mundo desde User1Controller";
+    }
+}
