48e956bf0bcba2fb0c2e223b558f11737667c987
ChrisAle117 <20223tn105@utez.edu.mx>
2025-05-26 17:45:51 -0600

    [FIRST COMMIT] Subida inicial del proyecyo

---
+++ src/main/java/mx/edu/utez/cvcaqr9a/Cvcaqr9aApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cvcaqr9a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvcaqr9aApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvcaqr9aApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/edu/utez/cvcaqr9a/Cvcaqr9aApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cvcaqr9a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cvcaqr9aApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
